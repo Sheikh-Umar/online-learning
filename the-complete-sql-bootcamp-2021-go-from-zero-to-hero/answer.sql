cc5d1ced84e1ada9048441c2e4e1b289a89cc8cd
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-01-19 15:19:06 +0800

    Added SQL code for section 5

--- the-complete-sql-bootcamp-2021-go-from-zero-to-hero/answer.sql
+++ the-complete-sql-bootcamp-2021-go-from-zero-to-hero/answer.sql
@@ -126,3 +126,26 @@
 
 
 	
+-- SECTION 5: JOINS
+-- Challenge: JOIN
+	-- Q1: California sales tax laws have changed and we need to alert our customers to this through email. What are the emails of the customers who live in California?
+	SELECT district, email
+	FROM customer
+	INNER JOIN address
+	ON customer.address_id = address.address_id
+	WHERE district = 'California';
+
+-- Q2: A customer walks in and is a huge fan of the actor “Nick Wahlberg” and wants to know which movies he is in. Get a list of all the movies “Nick Wahlberg” has been in.
+	SELECT film.title, actor.first_name, actor.last_name
+	FROM film
+	INNER JOIN film_actor
+	ON film.film_id = film_actor.film_id
+	INNER JOIN actor
+	ON actor.actor_id = film_actor.actor_id
+	WHERE actor.first_name = 'Nick'
+	AND actor.last_name = 'Wahlberg';
+
+
+
+
+
