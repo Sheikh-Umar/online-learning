ffa1a2d2980d2ea75bc814c8a269ae8c58addef4
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-01-26 11:53:51 +0800

    Added SQL code for Lesson 13 of SQLBolt

--- sqlbolt/sqlbolt-answers.sql
+++ sqlbolt/sqlbolt-answers.sql
@@ -301,3 +301,17 @@ GROUP BY DIRECTOR;
 
 
 
+
+-- SQL Lesson 13: Inserting Rows (Link: https://sqlbolt.com/lesson/inserting_rows)
+-- Exercise 13 - Tasks
+-- Q1: Add the studio's new production, Toy Story 4 to the list of movies (you can use any director) ✓
+INSERT INTO Movies VALUES
+(6, "Toy Story 4", "John Lasseter", 2016, 102);
+
+-- Q2: Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table. 
+INSERT INTO Boxoffice VALUES
+(6, 8.7, 340000000, 270000000);
+
+
+
+
