c9db2b6274b4b5fe3f7b10a7973b3aea6dfe949f
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-01-28 00:04:02 +0800

    Added SQL Lesson 15 Code

--- sql/sqlbolt/sqlbolt-answers.sql
+++ sql/sqlbolt/sqlbolt-answers.sql
@@ -335,3 +335,15 @@ WHERE Title = "Toy Story 8";
 
 
 
+-- SQL Lesson 15: Deleting rows (Link: https://sqlbolt.com/lesson/deleting_rows)
+-- Exercise 15 - Tasks
+-- Q1: This database is getting too big, lets remove all movies that were released before 2005.
+DELETE FROM Movies
+WHERE Year < 2005;
+
+-- Q2: Andrew Stanton has also left the studio, so please remove all movies directed by him. 
+DELETE FROM Movies
+WHERE Director = "Andrew Stanton";
+
+
+
