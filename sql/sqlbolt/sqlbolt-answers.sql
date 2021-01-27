6dc55ce0cc276739e6434e959cce6ae8301b1333
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-01-27 10:33:08 +0800

    Added SQL Lesson 14 to sqlbolt-answers.sql

--- sql/sqlbolt/sqlbolt-answers.sql
+++ sql/sqlbolt/sqlbolt-answers.sql
@@ -315,3 +315,23 @@ INSERT INTO Boxoffice VALUES
 
 
 
+-- SQL Lesson 14: Updating rows (Link: https://sqlbolt.com/lesson/updating_rows)
+-- Exercise 14 - Tasks
+-- Q1: The director for A Bug's Life is incorrect, it was actually directed by John Lasseter 
+UPDATE Movies
+SET Director = "John Lasseter"
+WHERE Title = "A Bug's Life";
+
+-- Q2: The year that Toy Story 2 was released is incorrect, it was actually released in 1999 
+UPDATE Movies
+SET Year = 1999
+WHERE Title = "Toy Story 2";
+
+-- Q3: Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was — directed by Lee Unkrich 
+UPDATE Movies
+SET Title = "Toy Story 3",
+    	Director = "Lee Unkrich"
+WHERE Title = "Toy Story 8";
+
+
+
