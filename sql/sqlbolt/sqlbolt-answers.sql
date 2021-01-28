5ab8c6e112251dfe7c68dbb3cfa3978b75612aec
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-01-28 15:26:21 +0800

    Added SQL Lesson 15 Code

--- sql/sqlbolt/sqlbolt-answers.sql
+++ sql/sqlbolt/sqlbolt-answers.sql
@@ -347,3 +347,15 @@ WHERE Director = "Andrew Stanton";
 
 
 
+-- SQL Lesson 15: Deleting rows (Link: https://sqlbolt.com/lesson/deleting_rows)
+-- Exercise 15 - Tasks
+-- Q1: This database is getting too big, lets remove all movies that were released before 2005.
+DELETE FROM Movies
+WHERE Year < 2005;
+
+-- Q2: Andrew Stanton has also left the studio, so please remove all movies directed by him. 
+DELETE FROM Movies
+WHERE Director = "Andrew Stanton";
+
+
+
